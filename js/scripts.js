59983755674dda36e76c3abead9fd7a322d7ea9e
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-20 15:10:56 -0800

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 // business logic
 
+// Game - constructor
+function Game(playerOne, playerTwo, turnScoreTotal, turn, winner) {
+  this.playerOne = new Player(),
+  this.playerTwo = new Player(),
+  this.turnScoreTotal = turnScoreTotal, 
+  this.turn = turn, // roll 1 - switch turn and lose points
+  this.winner = winner
+}
+
+// Player - constructor
+function Player(score) {
+  this.score = 0;
+}
+
 // dice object
 // dice.roll(); <-------how to call roll function
 let dice = {
@@ -10,9 +24,16 @@ let dice = {
   }
 }
 
-// Player Constructor
-function Player(score) {
-  this.score = 0;
+// add to turn score total - unheld (unsolidified points - could roll 1)
+Game.prototype.unclaimedTurnPoints = dice {
+  this.turnScoreTotal = turnScoreTotal + dice.roll();
 }
+
+
+// add turn total to score - hold (decide to end turn)
+
+// switch turn - roll 1
+
+// check winner - anyone have 100?
 
 // user interface logic
