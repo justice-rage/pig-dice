31d6c22ba5fd83109c0f13ad69539b33afb5f354
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-20 11:15:27 -0800

    add dice object

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 // business logic
 
+// dice object
+// dice.roll(); <-------how to call roll function
+let dice = {
+  sides: 6,
+  roll: function() {
+    let randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
+// Starting Score: player 1 score = 0
+// Starting Score: player 2 score = 0
 
 
 // user interface logic
